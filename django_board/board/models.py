cadfe1c6ce12ac95b58d9aa29a1e408fd88430a7
ksm0207 <ksm03071@naver.com>
2020-08-25 11:56:48 +0900

    게시판 페이지 추가
    - None

--- django_board/board/models.py
+++ django_board/board/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+# 가장 해야할것
+# 첫번째 게시글을 만들어보자 --> 제목 글 내용 작성자 등
+
+
+class Post(models.Model):
+    # 게시글(post) -->  제목 (postname) 내용(contents) model 생성
+    postname = models.CharField(max_length=50)
+    contents = models.TextField()
+
+    # 게시글의 제목이 Post object(1) (2) 대신 아래함수 사용
+    def __str__(self):
+        return self.postname
--- django_board/board/views.py
+++ django_board/board/views.py
@@ -1,7 +1,21 @@
 from django.shortcuts import render
+from .models import Post
 
-# Create your views here.
+
+# render : html템플릿을 렌더링을 해준다 아래의 index 라는 함수는 서버에 요청이 들어왔을 시 index.html을
+# 렌더링 해서 나에게 보여주는 역활을함
 
 
+# index.html 페이지를 부르는 함수
 def index(request):
     return render(request, "main/index.html")
+
+
+# board.html 페이지를 부르는 함수
+def board(request):
+
+    # Post를 가져와 postlist 변수에 저장
+    postlist = Post.objects.all()
+    print(postlist)
+    # board.html 페이지를 열때 모든 Post인 postlist도 같이 가져옴
+    return render(request, "main/board.html", {"postlist": postlist})
